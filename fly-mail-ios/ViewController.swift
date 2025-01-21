a070d5516c62f7ad4607d2730cc7ce0234de0a3a
karinahri <dolmatovakarina72@gmail.com>
2025-01-21 11:49:07 +0200

    Initial Commit

---
+++ fly-mail-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fly-mail-ios
+//
+//  Created by Karina Dolmatova on 21.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
